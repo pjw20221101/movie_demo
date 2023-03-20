c579fe13c5320b227f32c5bbc98b39f5788419ee
Park JW <p.jangwoo@gmail>
2023-03-20 13:24:37 +0900

    Movie 3월 20일 점심

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { dummy } from './movieDummy'; 
+import Movie from "./componts/Movie";
+
+function App() {
+  return (
+    <div>
+      <div className="app-container">
+        
+        {dummy.results.map((item) => {
+
+          return (
+            <Movie 
+              title = {item.title}
+              poster_path = {item.poster_path}
+              vote_average = {item.vote_average}
+            
+            /> 
+          ); 
+
+        })}
+      
+      </div>
+    </div>
+  );
+}
+
+export default App;
